7874eb71554584563436e8fb232c1c45a5aba936
Tomaz Gomes <tomgm1502@gmail.com>
2022-02-28 22:44:30 -0300

    done kan extension

--- src/Course/Extend.hs
+++ src/Course/Extend.hs
@@ -33,8 +33,7 @@ instance Extend ExactlyOne where
     (ExactlyOne a -> b)
     -> ExactlyOne a
     -> ExactlyOne b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance ExactlyOne"
+  (<<=) f a = ExactlyOne (f a)
 
 -- | Implement the @Extend@ instance for @List@.
 --
@@ -51,8 +50,8 @@ instance Extend List where
     (List a -> b)
     -> List a
     -> List b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance List"
+  (<<=) _ Nil = Nil
+  (<<=) f l@(_:.as) = f l :. (<<=) f as
 
 -- | Implement the @Extend@ instance for @Optional@.
 --
@@ -66,8 +65,8 @@ instance Extend Optional where
     (Optional a -> b)
     -> Optional a
     -> Optional b
-  (<<=) =
-    error "todo: Course.Extend (<<=)#instance Optional"
+  (<<=) f Empty      = Empty
+  (<<=) f o@(Full x) = Full (f o)
 
 -- | Duplicate the functor using extension.
 --
@@ -86,5 +85,4 @@ cojoin ::
   Extend k =>
   k a
   -> k (k a)
-cojoin =
-  error "todo: Course.Extend#cojoin"
+cojoin = (<<=) id
